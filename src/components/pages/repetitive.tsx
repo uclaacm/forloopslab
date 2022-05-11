b5dfeba28f9076bfb024ec14849439e6a76ebcb9
Annie <annietran1844@gmail.com>
2022-05-10 18:48:20 -0700

    fixed lint errors

--- src/components/pages/repetitive.tsx
+++ src/components/pages/repetitive.tsx
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import { render } from 'react-dom';
-import { propTypes } from 'react-markdown';
 
 // interface RepetitiveProps {
 //   onCorrect: () => void;
@@ -16,52 +14,52 @@ class Instruction {
 function DisplayCount(props:any){
   return (
     <div className = "displayCount">
-      <p>{props.name}: {props.number}</p>
+      <p>{props.name}</p>
     </div>
   );
 }
 
 function Repetitive(): JSX.Element {
-  const [numClicks, setNumClicks] = useState(0); //keep track of number of instructions
+  const [, setNumClicks] = useState(0); //keep track of number of instructions
   const[counts, setCounts]= useState([]); //array of classes instructions
-  
+
   const handleClick = (arg:any) => {
     //increment the instruction counter if button is clicked
     let count:any;
     setNumClicks((prev)=>{
-      count = prev+1
+      count = prev+1;
       return count;
     });
     //add new count to the array if button is clicked
     setCounts((prev) => {
-      let newArr = prev.slice();
+      const newArr = prev.slice();
       const item = new Instruction(arg,count);
       newArr.push(item);
       return newArr;
-    })
-  }
+    });
+  };
 
   const renderCounts = counts.map((counter)=>{
-      return <DisplayCount name = {counter.type} number = {counter.count} />
+    return <DisplayCount key = {counter.count} name = {counter.type}/>;
   });
-  
+
   return (
     <div className="frame">
       <div id="Repetitive">
-        <div> 
-         {renderCounts}
+        <div>
+          {renderCounts}
         </div>
         <div>
           <button onClick = {
-            ()=>{handleClick("Move Forward");}
+            ()=>{handleClick('Move Forward');}
           }>Move Forward</button>
         </div>
         <div>
           <button onClick = {
-            ()=>{handleClick("Turn Left");}
+            ()=>{handleClick('Turn Left');}
           }>Turn Left</button>
           <button onClick = {
-            ()=>{handleClick("Turn Right");}
+            ()=>{handleClick('Turn Right');}
           }>Turn Right</button>
         </div>
       </div>
@@ -70,3 +68,4 @@ function Repetitive(): JSX.Element {
 }
 
 export default Repetitive;
+
