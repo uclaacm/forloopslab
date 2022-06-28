0a75ebce1bee503c953fe0aa9fa5cc82053780e6
zhangallison <alzhang19@ucla.edu>
2022-06-27 18:27:01 -0700

    refactored to reduce redundancy

--- src/components/pages/pseudofill.tsx
+++ src/components/pages/pseudofill.tsx
@@ -6,26 +6,52 @@ import { GeneralDropdown } from '../shared/generalDropdown';
 // onCorrect: () => void;
 // }
 
+interface MoveFillProps {
+  onChange: (value:string, index:number) => void;
+  index: number;
+}
+
+interface TurnFillProps {
+  onChange: (value:string, index:number) => void;
+  index: number;
+}
+
+function MoveFill(props:MoveFillProps): JSX.Element {
+  return (
+    <div className='line'>
+      <div>Move forward</div>
+      <input placeholder="3" onChange={(val) => props.onChange(val.target.value, props.index)}></input>
+      <div>steps</div>
+    </div>
+  );
+}
+
+function TurnFill(props:TurnFillProps): JSX.Element {
+  return (
+    <div className='line'>
+      <div>Turn</div>
+      <div>
+        <GeneralDropdown options={['right','left','up', 'down']} onChange={(value) => props.onChange(value, props.index)} position="top"/>
+      </div>
+    </div>
+  );
+}
+
 function PseudoFill(): JSX.Element {
 
-  const [fillValue1, setFillValue1] = useState('');
-  const [fillValue2, setFillValue2] = useState('');
-  const [fillValue3, setFillValue3] = useState('');
-  const [fillValue4, setFillValue4] = useState('');
-  const [dropValue1, setDropValue1] = useState('');
-  const [dropValue2, setDropValue2] = useState('');
-  const [dropValue3, setDropValue3] = useState('');
-  const [dropValue4, setDropValue4] = useState('');
+  const initialFillValues = ['','','',''];
+  const initialDropValues = ['','','',''];
 
-  const fillOnChange1 = (value:string) => { setFillValue1(value); };
-  const fillOnChange2 = (value:string) => { setFillValue2(value); };
-  const fillOnChange3 = (value:string) => { setFillValue3(value); };
-  const fillOnChange4 = (value:string) => { setFillValue4(value); };
+  const [fillValues, setFillValues] = useState(initialFillValues);
+  const [dropValues, setDropValues] = useState(initialDropValues);
 
-  const dropOnChange1 = (value:string) => { setDropValue1(value); };
-  const dropOnChange2 = (value:string) => { setDropValue2(value); };
-  const dropOnChange3 = (value:string) => { setDropValue3(value); };
-  const dropOnChange4 = (value:string) => { setDropValue4(value); };
+  const fillOnChange = (value:string, index:number) => {
+    setFillValues({...fillValues, [index]: value});
+  };
+
+  const dropOnChange = (value:string, index:number) => {
+    setDropValues({...dropValues, [index]: value});
+  };
 
   return (
     <div className="frame">
@@ -35,64 +61,28 @@ function PseudoFill(): JSX.Element {
         <div id="instructions-title">Instructions</div>
         <div id="instructions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</div>
         <div id="content">
-          <div className='line'>
-            <div>Move forward</div>
-            <input placeholder="3" onChange={(val) => fillOnChange1(val.target.value)}></input>
-            <div>steps</div>
-          </div>
-          <div className='line'>
-            <div>Turn</div>
-            <div>
-              <GeneralDropdown options={['right','left','up', 'down']} onChange={(value) => dropOnChange1(value)} position="top"/>
-            </div>
-          </div>
-          <div className='line'>
-            <div>Move forward</div>
-            <input placeholder="3" onChange={(val) => fillOnChange2(val.target.value)}></input>
-            <div>steps</div>
-          </div>
-          <div className='line'>
-            <div>Turn</div>
-            <div>
-              <GeneralDropdown options={['right','left','up', 'down']} onChange={(value) => dropOnChange2(value)} position="top"/>
-            </div>
-          </div>
-          <div className='line'>
-            <div>Move forward</div>
-            <input placeholder="3" onChange={(val) => fillOnChange3(val.target.value)}></input>
-            <div>steps</div>
-          </div>
-          <div className='line'>
-            <div>Turn</div>
-            <div>
-              <GeneralDropdown options={['right','left','up', 'down']} onChange={(value) => dropOnChange3(value)} position="top"/>
-            </div>
-          </div>
-          <div className='line'>
-            <div>Move forward</div>
-            <input placeholder="3" onChange={(val) => fillOnChange4(val.target.value)}></input>
-            <div>steps</div>
-          </div>
-          <div className='line'>
-            <div>Turn</div>
-            <div>
-              <GeneralDropdown options={['right','left','up', 'down']} onChange={(value) => dropOnChange4(value)} position="top"/>
-            </div>
-          </div>
+          <MoveFill onChange={fillOnChange} index={0}/>
+          <TurnFill onChange={dropOnChange} index={0}/>
+          <MoveFill onChange={fillOnChange} index={1}/>
+          <TurnFill onChange={dropOnChange} index={1}/>
+          <MoveFill onChange={fillOnChange} index={2}/>
+          <TurnFill onChange={dropOnChange} index={2}/>
+          <MoveFill onChange={fillOnChange} index={3}/>
+          <TurnFill onChange={dropOnChange} index={3}/>
         </div>
         <button id="run">Run</button>
         <button id="reset">Reset</button>
         <button id="continue">Continue</button>
       </div>
       <div id="main">
-        <div>{fillValue1}</div>
-        <div>{dropValue1}</div>
-        <div>{fillValue2}</div>
-        <div>{dropValue2}</div>
-        <div>{fillValue3}</div>
-        <div>{dropValue3}</div>
-        <div>{fillValue4}</div>
-        <div>{dropValue4}</div>
+        <div>{fillValues[0]}</div>
+        <div>{dropValues[0]}</div>
+        <div>{fillValues[1]}</div>
+        <div>{dropValues[1]}</div>
+        <div>{fillValues[2]}</div>
+        <div>{dropValues[2]}</div>
+        <div>{fillValues[3]}</div>
+        <div>{dropValues[3]}</div>
       </div>
     </div>
 
