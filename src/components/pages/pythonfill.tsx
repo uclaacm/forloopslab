994ea4bbcdfddb2cf1357c1a862336474d80e83d
zhangallison <alzhang19@ucla.edu>
2022-07-05 17:37:33 -0700

    moved pythontype code to correct page

--- src/components/pages/pythonfill.tsx
+++ src/components/pages/pythonfill.tsx
@@ -14,23 +14,7 @@ function PythonFill(): JSX.Element {
         <div id="level-title">Level Title</div>
         <div id="instructions-title">Instructions</div>
         <div id="instructions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</div>
-        <div id="content">
-          <div id="code">
-            {codeContent.map((item) => {
-              /*let indents: any = 0;
-              for (let i = 0; i < item.length; i++) {
-                if (item[i] == ' ') indents += 1;
-                else break;
-              }*/
-              return (
-                <div key={item}>{item}</div>
-              );
-            },
-            )}
-          </div>
-          <div>Your code here: </div>
-          <textarea id="code-input"></textarea>
-        </div>
+        <div id="content">INSERT SIDEBAR CONTENT HERE</div>
         <button id="run">Run</button>
         <button id="reset">Reset</button>
         <button id="continue">Continue</button>
--- src/components/pages/pythontype.tsx
+++ src/components/pages/pythontype.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 // }
 
 function PythonType(): JSX.Element {
+  const codeContent = ['for steps in range(3):', ' moveForward()', 'turnLeft()'];
+
   return (
     <div className="frame">
       <div id="sidebar">
@@ -12,7 +14,23 @@ function PythonType(): JSX.Element {
         <div id="level-title">Level Title</div>
         <div id="instructions-title">Instructions</div>
         <div id="instructions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</div>
-        <div id="content">INSERT SIDEBAR CONTENT HERE</div>
+        <div id="content">
+          <div id="code">
+            {codeContent.map((item) => {
+              /*let indents: any = 0;
+              for (let i = 0; i < item.length; i++) {
+                if (item[i] == ' ') indents += 1;
+                else break;
+              }*/
+              return (
+                <div key={item}>{item}</div>
+              );
+            },
+            )}
+          </div>
+          <div>Your code here: </div>
+          <textarea id="code-input"></textarea>
+        </div>
         <button id="run">Run</button>
         <button id="reset">Reset</button>
         <button id="continue">Continue</button>
