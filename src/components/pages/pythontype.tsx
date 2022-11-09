5a81f3db1a99dce566ebfdf2ff069eb157ac8beb
Annie Tran <88398285+abt14@users.noreply.github.com>
2022-11-08 18:45:13 -0800

    lint-fix

--- src/components/pages/pythontype.tsx
+++ src/components/pages/pythontype.tsx
@@ -1,141 +1,156 @@
-import { faRotateLeft, faPlay} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link, useLocation } from 'react-router-dom';
-import '../../styles/pythontype.scss';
-import {Boxes} from '../shared/Boxes';
-import { Maze } from '../shared/maze';
-import { Robot } from '../shared/Robot';
-import '../../styles/app.scss';
-import '../../styles/levelSelect.scss';
+/*eslint-disable quotes*/
+import { faRotateLeft, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, useLocation } from "react-router-dom";
+import "../../styles/pythontype.scss";
+import { Boxes } from "../shared/Boxes";
+import { Maze } from "../shared/maze";
+// import { Robot } from '../shared/Robot';
+import "../../styles/app.scss";
+import "../../styles/levelSelect.scss";
 
-function SampleSyntax(){
-  return(
-    <div className='sampleSyntax'>
-      <div><span style = {{color: '#F08000'}}>for </span>steps
-        <span style = {{color: '#F08000'}}> in </span>
-        <span style = {{color: '#EC5800'}}>range</span>(
-        <span style = {{color: '#89CFF0'}}>3</span>):</div>
-      <div style={{marginLeft: 25}}>moveForward()</div>
+function SampleSyntax() {
+  return (
+    <div className="sampleSyntax">
+      <div>
+        <span style={{ color: "#F08000" }}>for </span>steps
+        <span style={{ color: "#F08000" }}> in </span>
+        <span style={{ color: "#EC5800" }}>range</span>(
+        <span style={{ color: "#89CFF0" }}>3</span>):
+      </div>
+      <div style={{ marginLeft: 25 }}>moveForward()</div>
       <div>turnForward()</div>
     </div>
   );
 }
 
-function PythonType(props: {
-  pages: string[]
-}): JSX.Element {
-
+function PythonType(props: { pages: string[] }): JSX.Element {
   const location = useLocation();
   const current = location.pathname;
   const currPage = props.pages.indexOf(current);
 
-
-  const boxes = Boxes(4,5);
+  const boxes = Boxes(4, 5);
   // const codeContent = ['for steps in range(3):', ' moveForward()', 'turnLeft()'];
 
-  const calculateKeyframes = (codedInstructionsProps: (string | number)[]) => {
-    const xArr = [0];
-    const yArr = [0];
-    let direction = 'right';
-    codedInstructionsProps.forEach((item) => {
-      if (item == 'right' || item == 'left'){
-        if (direction == 'right'){
-          if (item=='right'){
-            direction = 'down';
-          }
-          else{
-            direction = 'up';
-          }
-        }
-        else if (direction == 'down'){
-          if (item=='right'){
-            direction = 'left';
-          }
-          else{
-            direction = 'right';
-          }
-        }
-        else if (direction == 'left'){
-          if (item=='right'){
-            direction = 'up';
-          }
-          else{
-            direction = 'down';
-          }
-        }
-        else if (direction == 'up'){
-          if (item=='right'){
-            direction = 'right';
-          }
-          else{
-            direction = 'left';
-          }
-        }
-      }
-      else{
-        if (direction == 'right'){
-          xArr.push(xArr[xArr.length - 1] + (+item*100));
-          yArr.push(yArr[yArr.length - 1]);
-        }
-        else if (direction == 'left'){
-          xArr.push(xArr[xArr.length - 1] - (+item*100));
-          yArr.push(yArr[yArr.length - 1]);
-        }
-        else if (direction == 'down'){
-          xArr.push(xArr[xArr.length - 1]);
-          yArr.push(yArr[yArr.length - 1] + (+item*100));
-        }
-        else if (direction == 'up'){
-          xArr.push(xArr[xArr.length - 1]);
-          yArr.push(yArr[yArr.length - 1] - (+item*100));
-        }
-      }
-    });
-    return [xArr, yArr];
-  };
+  // const calculateKeyframes = (codedInstructionsProps: (string | number)[]) => {
+  //   const xArr = [0];
+  //   const yArr = [0];
+  //   let direction = 'right';
+  //   codedInstructionsProps.forEach((item) => {
+  //     if (item == 'right' || item == 'left'){
+  //       if (direction == 'right'){
+  //         if (item=='right'){
+  //           direction = 'down';
+  //         }
+  //         else{
+  //           direction = 'up';
+  //         }
+  //       }
+  //       else if (direction == 'down'){
+  //         if (item=='right'){
+  //           direction = 'left';
+  //         }
+  //         else{
+  //           direction = 'right';
+  //         }
+  //       }
+  //       else if (direction == 'left'){
+  //         if (item=='right'){
+  //           direction = 'up';
+  //         }
+  //         else{
+  //           direction = 'down';
+  //         }
+  //       }
+  //       else if (direction == 'up'){
+  //         if (item=='right'){
+  //           direction = 'right';
+  //         }
+  //         else{
+  //           direction = 'left';
+  //         }
+  //       }
+  //     }
+  //     else{
+  //       if (direction == 'right'){
+  //         xArr.push(xArr[xArr.length - 1] + (+item*100));
+  //         yArr.push(yArr[yArr.length - 1]);
+  //       }
+  //       else if (direction == 'left'){
+  //         xArr.push(xArr[xArr.length - 1] - (+item*100));
+  //         yArr.push(yArr[yArr.length - 1]);
+  //       }
+  //       else if (direction == 'down'){
+  //         xArr.push(xArr[xArr.length - 1]);
+  //         yArr.push(yArr[yArr.length - 1] + (+item*100));
+  //       }
+  //       else if (direction == 'up'){
+  //         xArr.push(xArr[xArr.length - 1]);
+  //         yArr.push(yArr[yArr.length - 1] - (+item*100));
+  //       }
+  //     }
+  //   });
+  //   return [xArr, yArr];
+  // };
 
   return (
     <div className="frame">
       <div id="sidebar">
         <div id="level-title">Python Type</div>
-        <div id="instructions">Give the robot instructions to navigate the maze. Make sure you don&apos;t run into any obstacles!.</div>
+        <div id="instructions">
+          Give the robot instructions to navigate the maze. Make sure you
+          don&apos;t run into any obstacles!.
+        </div>
         <div id="content">
           <div id="code">
-            <SampleSyntax/>
+            <SampleSyntax />
           </div>
           <div>Your code here: </div>
-          <textarea id="code-input" spellCheck="false" ></textarea>
+          <textarea id="code-input" spellCheck="false"></textarea>
         </div>
       </div>
       <div id="main">
         <div className="main-section">
           <div id="title">LoopBots</div>
           <div className="level-select">
-            {currPage != 0 && <Link to={props.pages[currPage-1]} className="level-select-button left">&#9664;</Link>}
-            Level {currPage+1} of 5
-            {currPage != props.pages.length - 1 && <Link to={props.pages[currPage+1]} className="level-select-button right">&#9654;</Link>}
+            {currPage != 0 && (
+              <Link
+                to={props.pages[currPage - 1]}
+                className="level-select-button left"
+              >
+                &#9664;
+              </Link>
+            )}
+            Level {currPage + 1} of 5
+            {currPage != props.pages.length - 1 && (
+              <Link
+                to={props.pages[currPage + 1]}
+                className="level-select-button right"
+              >
+                &#9654;
+              </Link>
+            )}
           </div>
         </div>
         <div id="content">
-          <Maze rows={4} cols={6} boxCoords={boxes}/>
+          <Maze rows={4} cols={6} boxCoords={boxes} />
           {/* <Robot keyframes={calculateKeyframes(codedInstructions)}></Robot> */}
         </div>
         <div className="main-section">
           <div id="footer">made with ♥ by acm.teachla</div>
           <div id="buttons">
-            <button id="run" className='control-btn'>
+            <button id="run" className="control-btn">
               <FontAwesomeIcon icon={faPlay} />
             </button>
-            <button id="reset" className='control-btn'>
+            <button id="reset" className="control-btn">
               <FontAwesomeIcon icon={faRotateLeft} />
             </button>
           </div>
         </div>
-
       </div>
     </div>
   );
 }
 
 export default PythonType;
-
+/*eslint-enable quotes*/
--- src/components/shared/Robot.tsx
+++ src/components/shared/Robot.tsx
@@ -1,13 +1,16 @@
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import robotRight from '../../assets/RobotDemo.svg';
+/*eslint-disable quotes*/
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import robotRight from "../../assets/RobotDemo.svg";
 
 // interface Frame {
 //   x: (string|number)[],
 //   y: (string|number)[]
 // }
 
-export function Robot(props: {keyframes: (string|number)[][]}):JSX.Element{
+export function Robot(props: {
+  keyframes: (string | number)[][];
+}): JSX.Element {
   // let xArr = [0]
   // let yArr = [0]
   // let direction = 'right';
@@ -78,15 +81,14 @@ export function Robot(props: {keyframes: (string|number)[][]}):JSX.Element{
   //   y: [0]
   // })
 
-  const [test,setTest] = useState<(number)>(0);
-  const [xArr,setXArr] = useState<(string|number)[]>([0]);
-  const [yArr,setYArr] = useState<(string|number)[]>([0]);
-
+  const [test, setTest] = useState<number>(0);
+  const [xArr, setXArr] = useState<(string | number)[]>([0]);
+  const [yArr, setYArr] = useState<(string | number)[]>([0]);
 
   useEffect(() => {
-    console.log('Keyframe updated!');
+    console.log("Keyframe updated!");
     //console.log(`(${props.keyframes[0]},${props.keyframes[1]})`)
-    if(props.keyframes) {
+    if (props.keyframes) {
       setXArr(props.keyframes[0]);
       setYArr(props.keyframes[1]);
     }
@@ -98,36 +100,30 @@ export function Robot(props: {keyframes: (string|number)[][]}):JSX.Element{
     //   y: props.keyframes[1]
     // })
     // console.log(props.keyframes[1])
-    setTest(test+1);
+    setTest(test + 1);
   }, [props.keyframes]);
 
-  if(JSON.stringify(xArr) === JSON.stringify([0]) && JSON.stringify(yArr) === JSON.stringify([0])) {
-    return(
-      <img
-        className = "robot"
-        src = {robotRight}
-        alt = "robot">
-      </img>
-
-    );
-  }
-  else {
+  if (
+    JSON.stringify(xArr) === JSON.stringify([0]) &&
+    JSON.stringify(yArr) === JSON.stringify([0])
+  ) {
+    return <img className="robot" src={robotRight} alt="robot"></img>;
+  } else {
     console.log(xArr, yArr);
-    return(
+    return (
       <motion.img
-        animate = {{
+        animate={{
           x: xArr,
           y: yArr,
           // x: [0, 0],
           // y: [0, 100]
         }}
         key={test}
-        className = "robot"
-        src = {robotRight}
-        alt = "robot">
-      </motion.img>
-
+        className="robot"
+        src={robotRight}
+        alt="robot"
+      ></motion.img>
     );
   }
-
 }
+/*eslint-enable quotes*/
