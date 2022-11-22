d2821215f5e9ab3a24c5f667588bcd94b335e562
Annie Tran <88398285+abt14@users.noreply.github.com>
2022-11-21 20:26:52 -0800

    robot positioning fixed

--- src/components/pages/pythontype.tsx
+++ src/components/pages/pythontype.tsx
@@ -1,16 +1,16 @@
 /*eslint-disable quotes*/
+import { python } from "@codemirror/lang-python";
 import { faRotateLeft, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState, useEffect } from 'react';
+import CodeMirror from "@uiw/react-codemirror";
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../../styles/pythontype.scss";
 //import { Boxes } from "../shared/Boxes";
 import { Maze } from "../shared/maze";
-// import { Robot } from '../shared/Robot';
+import { Robot } from "../shared/Robot";
 import "../../styles/app.scss";
 import "../../styles/levelSelect.scss";
-import CodeMirror from '@uiw/react-codemirror';
-import { python } from '@codemirror/lang-python';
 
 function SampleSyntax() {
   return (
@@ -32,11 +32,11 @@ function PythonType(props: { pages: string[] }): JSX.Element {
   const current = location.pathname;
   const currPage = props.pages.indexOf(current);
 
-  const [code, setCode] = useState('');
-  const initMovement:(string | number)[] = [];
+  const [code, setCode] = useState("");
+  const initMovement: (string | number)[] = [];
   const [movement, setMovement] = useState(initMovement);
 
-  const onChange = (value:string) => {
+  const onChange = (value: string) => {
     setCode(value);
   };
 
@@ -46,84 +46,73 @@ function PythonType(props: { pages: string[] }): JSX.Element {
     //console.log(lines);
     for (let i = 0; i < lines.length; i++) {
       const cur = lines[i];
-      if (cur.substring(0,3) == 'for') {
+      if (cur.substring(0, 3) == "for") {
         // for steps in range(3)
         const numSteps = parseInt(cur.substring(19, cur.length - 2));
-        setMovement(moves => moves.concat(numSteps));
-      }
-      else if (cur == 'turnLeft()') {
-        setMovement(moves => moves.concat('left'));
-      }
-      else if (cur == 'turnRight()') {
-        setMovement(moves => moves.concat('right'));
+        setMovement((moves) => moves.concat(numSteps));
+      } else if (cur == "turnLeft()") {
+        setMovement((moves) => moves.concat("left"));
+      } else if (cur == "turnRight()") {
+        setMovement((moves) => moves.concat("right"));
       }
     }
   };
 
-  useEffect(() => { console.log(movement); }, [movement]);
+  useEffect(() => {
+    console.log(movement);
+  }, [movement]);
 
   // const codeContent = ['for steps in range(3):', ' moveForward()', 'turnLeft()'];
 
-  // const calculateKeyframes = (codedInstructionsProps: (string | number)[]) => {
-  //   const xArr = [0];
-  //   const yArr = [0];
-  //   let direction = 'right';
-  //   codedInstructionsProps.forEach((item) => {
-  //     if (item == 'right' || item == 'left'){
-  //       if (direction == 'right'){
-  //         if (item=='right'){
-  //           direction = 'down';
-  //         }
-  //         else{
-  //           direction = 'up';
-  //         }
-  //       }
-  //       else if (direction == 'down'){
-  //         if (item=='right'){
-  //           direction = 'left';
-  //         }
-  //         else{
-  //           direction = 'right';
-  //         }
-  //       }
-  //       else if (direction == 'left'){
-  //         if (item=='right'){
-  //           direction = 'up';
-  //         }
-  //         else{
-  //           direction = 'down';
-  //         }
-  //       }
-  //       else if (direction == 'up'){
-  //         if (item=='right'){
-  //           direction = 'right';
-  //         }
-  //         else{
-  //           direction = 'left';
-  //         }
-  //       }
-  //     }
-  //     else{
-  //       if (direction == 'right'){
-  //         xArr.push(xArr[xArr.length - 1] + (+item*100));
-  //         yArr.push(yArr[yArr.length - 1]);
-  //       }
-  //       else if (direction == 'left'){
-  //         xArr.push(xArr[xArr.length - 1] - (+item*100));
-  //         yArr.push(yArr[yArr.length - 1]);
-  //       }
-  //       else if (direction == 'down'){
-  //         xArr.push(xArr[xArr.length - 1]);
-  //         yArr.push(yArr[yArr.length - 1] + (+item*100));
-  //       }
-  //       else if (direction == 'up'){
-  //         xArr.push(xArr[xArr.length - 1]);
-  //         yArr.push(yArr[yArr.length - 1] - (+item*100));
-  //       }
-  //     }
-  //   });
-  //   return [xArr, yArr];
-  // };
+  const calculateKeyframes = (codedInstructionsProps: (string | number)[]) => {
+    const xArr = [0];
+    const yArr = [0];
+    let direction = "right";
+    codedInstructionsProps.forEach((item) => {
+      if (item == "right" || item == "left") {
+        if (direction == "right") {
+          if (item == "right") {
+            direction = "down";
+          } else {
+            direction = "up";
+          }
+        } else if (direction == "down") {
+          if (item == "right") {
+            direction = "left";
+          } else {
+            direction = "right";
+          }
+        } else if (direction == "left") {
+          if (item == "right") {
+            direction = "up";
+          } else {
+            direction = "down";
+          }
+        } else if (direction == "up") {
+          if (item == "right") {
+            direction = "right";
+          } else {
+            direction = "left";
+          }
+        }
+      } else {
+        if (direction == "right") {
+          xArr.push(xArr[xArr.length - 1] + +item * 100);
+          yArr.push(yArr[yArr.length - 1]);
+        } else if (direction == "left") {
+          xArr.push(xArr[xArr.length - 1] - +item * 100);
+          yArr.push(yArr[yArr.length - 1]);
+        } else if (direction == "down") {
+          xArr.push(xArr[xArr.length - 1]);
+          yArr.push(yArr[yArr.length - 1] + +item * 100);
+        } else if (direction == "up") {
+          xArr.push(xArr[xArr.length - 1]);
+          yArr.push(yArr[yArr.length - 1] - +item * 100);
+        }
+      }
+    });
+    return [xArr, yArr];
+  };
 
   return (
     <div className="frame">
@@ -171,14 +160,28 @@ function PythonType(props: { pages: string[] }): JSX.Element {
             )}
           </div>
         </div>
-        <div id="content">
-          <Maze rows={4} cols={6} boxCoords={[[1,0],[1,1],[0,3],[1,3],[2,3],[3,1],[2,5]]}/>
-          {/* <Robot keyframes={calculateKeyframes(codedInstructions)}></Robot> */}
+        <div className="main-section">
+          <div className="maze">
+            <Maze
+              rows={4}
+              cols={6}
+              boxCoords={[
+                [1, 0],
+                [1, 1],
+                [0, 3],
+                [1, 3],
+                [2, 3],
+                [3, 1],
+                [2, 5],
+              ]}
+            />
+            <Robot keyframes={calculateKeyframes(movement)}></Robot>
+          </div>
         </div>
         <div className="main-section">
           <div id="footer">made with ♥ by acm.teachla</div>
           <div id="buttons">
-            <button id="run" className='control-btn' onClick={handleRunClick}>
+            <button id="run" className="control-btn" onClick={handleRunClick}>
               <FontAwesomeIcon icon={faPlay} />
             </button>
             <button id="reset" className="control-btn">
