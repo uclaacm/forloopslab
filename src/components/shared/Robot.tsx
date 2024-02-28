497bdd2443e6f9b95f4223c5898fb5f23c69963e
naomi gong <naomigong2005@gmail.com>
2024-02-28 00:03:44 -0800

    fixed the css styling of landing page and randomized the boxes

--- src/components/shared/Robot.tsx
+++ src/components/shared/Robot.tsx
@@ -1,129 +1,101 @@
-/*eslint-disable quotes*/
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import robotRight from "../../assets/RobotDemo.svg";
+// import { motion } from "framer-motion";
+// import { useEffect, useState } from "react";
+// import robotRight from "../../assets/RobotDemo.svg";
 
-// interface Frame {
-//   x: (string|number)[],
-//   y: (string|number)[]
+// export function Robot(props: {
+//   keyframes: (string | number)[][];
+// }): JSX.Element {
+//   const [xArr, setXArr] = useState<(string | number)[]>([0]);
+//   const [yArr, setYArr] = useState<(string | number)[]>([0]);
+
+//   useEffect(() => {
+//     if (props.keyframes) {
+//       setXArr(props.keyframes[0]);
+//       setYArr(props.keyframes[1]);
+//     }
+//   }, [props.keyframes]);
+
+//   if (JSON.stringify(xArr) === JSON.stringify([0]) && JSON.stringify(yArr) === JSON.stringify([0])) {
+//     return <img className="robot" src={robotRight} alt="robot" />;
+//   } else {
+//     return (
+//       <motion.img
+//         animate={{
+//           x: xArr,
+//           y: yArr
+//         }}
+//         className="robot"
+//         src={robotRight}
+//         alt="robot"
+//       />
+//     );
+//   }
 // }
 
-export function Robot(props: {
+
+
+// import { motion } from "framer-motion";
+// import { useEffect, useState } from "react";
+// import robotRight from "../../assets/RobotDemo.svg";
+
+// export function Robot(props: {
+//   keyframes: (string | number)[][];
+// }): JSX.Element {
+//   const [xArr, setXArr] = useState<(string | number)[]>([0]);
+//   const [yArr, setYArr] = useState<(string | number)[]>([0]);
+
+//   useEffect(() => {
+//     if (props.keyframes) {
+//       setXArr(props.keyframes[0]);
+//       setYArr(props.keyframes[1]);
+//     }
+//   }, [props.keyframes]);
+
+//   if (JSON.stringify(xArr) === JSON.stringify([0]) && JSON.stringify(yArr) === JSON.stringify([0])) {
+//     return <img className="robot" src={robotRight} alt="robot" />;
+//   } else {
+//     return (
+//       <motion.img
+//         animate={{
+//           x: xArr.map(value => parseInt(value.toString())), // Convert string values to integers
+//           y: yArr.map(value => parseInt(value.toString())) // Convert string values to integers
+//         }}
+//         className="robot"
+//         src={robotRight}
+//         alt="robot"
+//       />
+//     );
+//   }
+// }
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import robotRight from "../../assets/RobotDemo.svg";
+
+function Robot(props: {
   keyframes: (string | number)[][];
 }): JSX.Element {
-  // let xArr = [0]
-  // let yArr = [0]
-  // let direction = 'right';
-
-  // function MoveRobot(item:string|number){
-  //   //change direction
-  //   if (item == 'right' || item == 'left'){
-  //     if (direction == 'right'){
-  //       if (item=='right'){
-  //         direction = 'down';
-  //       }
-  //       else{
-  //         direction = 'up';
-  //       }
-  //     }
-  //     else if (direction == 'down'){
-  //       if (item=='right'){
-  //         direction = 'left';
-  //       }
-  //       else{
-  //         direction = 'right';
-  //       }
-  //     }
-  //     else if (direction == 'left'){
-  //       if (item=='right'){
-  //         direction = 'up';
-  //       }
-  //       else{
-  //         direction = 'down';
-  //       }
-  //     }
-  //     else if (direction == 'up'){
-  //       if (item=='right'){
-  //         direction = 'right';
-  //       }
-  //       else{
-  //         direction = 'left';
-  //       }
-  //     }
-  //   }
-  //   else{
-  //     if (direction == 'right'){
-  //       xArr.push(xArr[xArr.length - 1] + (+item*100))
-  //       yArr.push(yArr[yArr.length - 1])
-  //     }
-  //     else if (direction == 'left'){
-  //       xArr.push(xArr[xArr.length - 1] - (+item*100))
-  //       yArr.push(yArr[yArr.length - 1])
-  //     }
-  //     else if (direction == 'down'){
-  //       xArr.push(xArr[xArr.length - 1])
-  //       yArr.push(yArr[yArr.length - 1] + (+item*100))
-  //     }
-  //     else if (direction == 'up'){
-  //       xArr.push(xArr[xArr.length - 1])
-  //       yArr.push(yArr[yArr.length - 1] - (+item*100))
-  //     }
-  //   }
-  // }
-
-  // props.arr.forEach((item)=>{
-  //   MoveRobot(item)
-  // })
-  // const [xArr,setXArr] = useState<(string|number)[]>([0]);
-  // const [yArr,setYArr] = useState<(string|number)[]>([0]);
-  // const [frames, setFrames] = useState<Frame>({
-  //   x: [0],
-  //   y: [0]
-  // })
-
-  const [test, setTest] = useState<number>(0);
   const [xArr, setXArr] = useState<(string | number)[]>([0]);
   const [yArr, setYArr] = useState<(string | number)[]>([0]);
 
   useEffect(() => {
-    console.log("Keyframe updated!");
-    //console.log(`(${props.keyframes[0]},${props.keyframes[1]})`)
     if (props.keyframes) {
       setXArr(props.keyframes[0]);
       setYArr(props.keyframes[1]);
     }
-
-    // console.log(xArr)
-    // console.log(yArr)
-    // setFrames({
-    //   x: props.keyframes[0],
-    //   y: props.keyframes[1]
-    // })
-    // console.log(props.keyframes[1])
-    setTest(test + 1);
   }, [props.keyframes]);
 
-  if (
-    JSON.stringify(xArr) === JSON.stringify([0]) &&
-    JSON.stringify(yArr) === JSON.stringify([0])
-  ) {
-    return <img className="robot" src={robotRight} alt="robot"></img>;
-  } else {
-    console.log(xArr, yArr);
-    return (
-      <motion.img
-        animate={{
-          x: xArr,
-          y: yArr,
-          // x: [0, 0],
-          // y: [0, 100]
-        }}
-        key={test}
-        className="robot"
-        src={robotRight}
-        alt="robot"
-      ></motion.img>
-    );
-  }
+  return (
+    <motion.img
+      animate={{
+        x: xArr.map(value => parseInt(value.toString())), // Convert string values to integers
+        y: yArr.map(value => parseInt(value.toString())) // Convert string values to integers
+      }}
+      className="robot"
+      src={robotRight}
+      alt="robot"
+    />
+  );
 }
-/*eslint-enable quotes*/
+
+export default Robot;
+
--- src/components/shared/maze.tsx
+++ src/components/shared/maze.tsx
@@ -1,29 +1,32 @@
+import React from 'react';
 import '../../styles/maze.scss';
 import boxImg from '../../assets/smallbox.svg';
 
 interface MazeProps {
   rows: number;
   cols: number;
-  boxCoords:JSX.Element[][];
+  boxCoords: number[][];
 }
 
-export function Maze(props:MazeProps):JSX.Element {
-  // const [move, SetMove] = React.useState(false);
-  const squares:JSX.Element[][] = [];
+export function Maze(props: MazeProps): JSX.Element {
+  const squares: JSX.Element[][] = [];
+
+  // Fill squares array with empty squares
   for (let i = 0; i < props.rows; i++) {
+    squares.push([]);
     for (let j = 0; j < props.cols; j++) {
-      if (!squares[i]) {
-        squares[i] = [<div className = "box" key={`${i}-${j}`}><img className="hidden" src={boxImg} alt="cardboard box"></img></div>];
-      }
-      else {
-        squares[i].push(<div className = "box" key={`${i}-${j}`}><img src={boxImg} className="hidden" alt="cardboard box"></img></div>);
-      }
+      squares[i].push(<div className="box" key={`${i}-${j}`} />);
     }
   }
+
+  // Add boxes to specified positions
   for (let i = 0; i < props.boxCoords.length; i++) {
-    const r = props.boxCoords[i][0];
-    const c = props.boxCoords[i][1];
-    squares[r][c] = <div className = "box"><img src={boxImg} alt="cardboard box"></img></div>;
+    const [r, c] = props.boxCoords[i];
+    squares[r][c] = (
+      <div className="box" key={`${r}-${c}`}>
+        <img src={boxImg} alt="cardboard box" />
+      </div>
+    );
   }
 
   const mazeStyle = {
@@ -33,7 +36,7 @@ export function Maze(props:MazeProps):JSX.Element {
 
   return (
     <div className="maze-container" style={mazeStyle}>
-      {squares}
+      {squares.map((row, rowIndex) => row.map((square, colIndex) => square))}
     </div>
   );
 }
